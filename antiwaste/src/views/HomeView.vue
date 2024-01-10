3dac99f7ed886583bbf2c29754e737d7786c3f9b
Heat Koemnak <heatkimnak@gmail.com>
2024-01-10 13:23:15 +0700

    modify by adding vuetify

--- antiwaste/src/views/HomeView.vue
+++ antiwaste/src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <template>
-  <div class="home">
-    <h2>This is Home Page</h2>
+  <HelloWorld />
+  <div class="text-center">
+    <v-btn color="primary">
+      Open Dialog
+
+      <v-dialog v-model="dialog" activator="parent" width="auto">
+        <v-card>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" block @click="dialog = false"
+              >Close Dialog</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-btn>
   </div>
 </template>
 
 <script>
-export default {};
+import { defineComponent } from 'vue';
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  name: 'HomeView',
+
+  components: {
+    HelloWorld,
+  },
+});
 </script>
