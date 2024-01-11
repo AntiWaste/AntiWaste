e13434e33f2abbc1bbfbf1c3cde7daa6e0feb38e
ssdn7 <somsodana9@gmail.com>
2024-01-11 14:27:05 +0700

    u

--- antiwaste/src/components/NavTop.vue
+++ antiwaste/src/components/NavTop.vue
@@ -9,7 +9,6 @@
               <a class="nav-link" href="/">Home</a>
             </li>
             <li class="nav-item">
-<<<<<<< HEAD
               <a href="#/login" class="nav-link">Login</a>
             </li>
             <li class="nav-item">
@@ -17,21 +16,6 @@
             </li>
             <li class="nav-item">
               <a href="#/calendar" class="nav-link">Calendar</a>
-=======
-              <a href="/trash" class="nav-link">Trash</a>
-            </li>
-            <li class="nav-item">
-              <a href="/recycle" class="nav-link">Recycle</a>
-            </li>
-            <li class="nav-item">
-              <a href="/event" class="nav-link">Event</a>
-            </li>
-            <li class="nav-item">
-              <a href="/contact" class="nav-link">Contact</a>
-            </li>
-            <li class="nav-item">
-              <a href="/register" class="nav-link">Sign up</a>
->>>>>>> 01aa8f161a1ad48446aae27ac3c62c741da9ae06
             </li>
           </ul>
         </div>
--- antiwaste/src/main.js
+++ antiwaste/src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -8,17 +7,3 @@ import { loadFonts } from './plugins/webfontloader';
 loadFonts();
 
 createApp(App).use(router).use(vuetify).mount('#app');
-=======
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-
-//import component
-import NavTop from "./components/NavTop.vue";
-const app = createApp(App);
-
-//add components to 'app'
-app.component("nav-top", NavTop);
-
-app.use(router).mount("#app");
->>>>>>> 01aa8f161a1ad48446aae27ac3c62c741da9ae06
