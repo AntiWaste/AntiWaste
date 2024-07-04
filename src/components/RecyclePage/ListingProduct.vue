cd23fbe8c6924ae385935ec81fc5a454e3c7d7c6
Pheak02 <saingsopheak02@gmail.com>
2024-07-04 23:28:47 +0700

    can display product from db

--- src/components/RecyclePage/ListingProduct.vue
+++ src/components/RecyclePage/ListingProduct.vue
@@ -13,132 +13,83 @@
           Back
         </button>
       </div>
-      <!-- Welcome Banner -->
-      <v-card max-height="230">
-        <v-img
-          class="text-white flex justify-center items-center"
-          height="230"
-          src="../../assets/recycledproduct/image_cover.jpg"
-          cover
+
+      <!-- Product Grid -->
+      <div class="grid grid-cols-2 md:grid-cols-4 gap-8 px-20 py-5">
+        <div
+          v-for="item in paginatedItems"
+          :key="item.id"
+          class="w-auto overflow-hidden rounded-xl border"
         >
-          <div class="flex flex-col justify-center items-center">
-            <div class="text-4xl font-bold mb-3">Discover Sustainable Solutions - Explore Our Range of Eco-Friendly Products</div>
-            <router-link to="/product-post">
-              <button class="mt-2 px-4 py-2 font-bold bg-green-600 border rounded-lg text-white hover:bg-green-800 hover:text-white focus:outline-none">
-                Sell Your Product Now
-              </button>
-            </router-link>
+          <img
+            class="object-cover h-64 w-full"
+            :src="item.image_url"
+            alt="Product Image"
+          />
+
+          <div class="p-4">
+            <h3 class="text-lg font-semibold">{{ item.productName }}</h3>
+            <div class="flex items-center text-sm text-gray-600">
+              <span class="me-1">{{ item.owner }}</span>
+              <!-- Replace with your actual rating component -->
+              <i class="text-red-500 mdi mdi-fire-circle text-sm"></i>
+            </div>
+            <div class="flex items-center mt-2">
+              <!-- Replace with your actual rating component -->
+              <div class="text-gray-600 ms-4">
+                {{ item.rating }} ({{ item.reviews }})
+              </div>
+            </div>
+            <p class="text-sm text-gray-600 mt-2">
+              Location: {{ item.location }}
+            </p>
+            <p class="text-sm text-gray-600">
+              Contact Number: {{ item.contactNumber }}
+            </p>
+            <hr class="my-4" />
+            <div class="text-lg font-semibold">${{ item.price }}</div>
+            <div>
+              <router-link :to="'/product-detail/' + item.id">
+                <button
+                  class="mt-2 px-4 py-2 text-sm text-green-600 border border-green-600 rounded-lg hover:bg-green-100 focus:outline-none"
+                >
+                  View Detail
+                </button>
+              </router-link>
+            </div>
           </div>
-        </v-img>
-      </v-card>
+        </div>
+      </div>
+
+      <!-- Pagination Controls -->
+      <div class="flex justify-center mt-5">
+        <button
+          :disabled="currentPage === 1"
+          @click="currentPage -= 1"
+          class="px-4 py-2 bg-gray-200 text-gray-700 hover:bg-gray-300 rounded-full mx-2 focus:outline-none"
+        >
+          Previous
+        </button>
+        <span class="text-gray-700">{{ currentPage }}</span>
+        <button
+          :disabled="currentPage === totalPages"
+          @click="currentPage += 1"
+          class="px-4 py-2 bg-gray-200 text-gray-700 hover:bg-gray-300 rounded-full mx-2 focus:outline-none"
+        >
+          Next
+        </button>
+      </div>
     </template>
-
-    <!-- Filter and Search Bar -->
-    <div class="bg-green-100">
-      <div class="px-4 py-2">
-        <div class="max-w-6xl mx-auto flex items-center justify-between">
-          <!-- Filter Menu -->
-          <div class="flex items-center space-x-4">
-            <button
-              class="text-gray-700 hover:text-green-500 focus:outline-none"
-              @click="filterByAll"
-            >
-              All
-            </button>
-            <button
-              class="text-gray-700 hover:text-green-500 focus:outline-none"
-              @click="filterByRecentlyViewed"
-            >
-              Recently Viewed
-            </button>
-            <button
-              class="text-gray-700 hover:text-green-500 focus:outline-none"
-              @click="filterByFavorites"
-            >
-              Favorites
-            </button>
-          </div>
-          <!-- Search Bar -->
-          <div class="flex items-center">
-            <input
-              v-model="searchQuery"
-              type="text"
-              class="w-64 px-4 py-2 border border-gray-300 rounded-l-lg focus:outline-none focus:border-green-500"
-              placeholder="Search products..."
-            />
-            <button
-              @click="searchProducts"
-              class="px-4 py-2 bg-green-500 text-white rounded-r-lg hover:bg-green-600 focus:outline-none"
-            >
-              Search
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Product Grid -->
-    <div class="grid grid-cols-2 md:grid-cols-4 gap-8 px-20 py-5">
-      <div v-for="item in paginatedItems" :key="item.id" class="w-auto overflow-hidden rounded-xl border">
-        <img
-          class="object-cover h-64 w-full"
-          :src="require(`../../assets/recycledproduct/${item.image}`)"
-          alt="Product Image"
-        />
-
-        <div class="p-4">
-          <h3 class="text-lg font-semibold">{{ item.title }}</h3>
-          <div class="flex items-center text-sm text-gray-600">
-            <span class="me-1">{{ item.store }}</span>
-            <i class="text-red-500 mdi mdi-fire-circle text-sm"></i>
-          </div>
-          <div class="flex items-center mt-2">
-            <!-- Replace with your actual rating component -->
-            <div class="text-gray-600 ms-4">{{ item.rating }} ({{ item.reviews }})</div>
-          </div>
-          <p class="text-sm text-gray-600 mt-2">Location: {{ item.location }}</p>
-          <p class="text-sm text-gray-600">Contact Number: {{ item.contact }}</p>
-          <hr class="my-4">
-          <div class="text-lg font-semibold">${{ item.price }}</div>
-          <div>
-            <router-link :to="'/product-detail/' + item.id">
-              <button class="mt-2 px-4 py-2 text-sm text-green-600 border border-green-600 rounded-lg hover:bg-green-100 focus:outline-none">
-                View Detail
-              </button>
-            </router-link>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Pagination Controls -->
-    <div class="flex justify-center mt-5">
-      <button
-        :disabled="currentPage === 1"
-        @click="currentPage -= 1"
-        class="px-4 py-2 bg-gray-200 text-gray-700 hover:bg-gray-300 rounded-full mx-2 focus:outline-none"
-      >
-        Previous
-      </button>
-      <span class="text-gray-700">{{ currentPage }}</span>
-      <button
-        :disabled="currentPage === totalPages"
-        @click="currentPage += 1"
-        class="px-4 py-2 bg-gray-200 text-gray-700 hover:bg-gray-300 rounded-full mx-2 focus:outline-none"
-      >
-        Next
-      </button>
-    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import LoadingSpinner from '../LoadingSpinner.vue';
-import { API_BASE_URL } from '@/config';
+import axios from "axios";
+import LoadingSpinner from "../LoadingSpinner.vue";
+import { API_BASE_URL } from "@/config";
 
 export default {
-  name: 'WelcomeCard',
+  name: "WelcomeCard",
   components: {
     LoadingSpinner,
   },
@@ -148,7 +99,7 @@ export default {
       items: [], // Store fetched products
       currentPage: 1,
       itemsPerPage: 8,
-      searchQuery: '', // Store search query
+      searchQuery: "", // Store search query
       filteredItems: [], // Store filtered items
     };
   },
@@ -160,7 +111,10 @@ export default {
     // Calculate paginated items based on currentPage and itemsPerPage
     paginatedItems() {
       const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-      return this.filteredItems.slice(startIndex, startIndex + this.itemsPerPage);
+      return this.filteredItems.slice(
+        startIndex,
+        startIndex + this.itemsPerPage
+      );
     },
   },
   mounted() {
@@ -168,38 +122,17 @@ export default {
   },
   methods: {
     fetchProducts() {
-      // Replace with your actual API endpoint
-      axios.get(`${API_BASE_URL}products`)
-        .then(response => {
+      axios
+        .get(`${API_BASE_URL}products`)
+        .then((response) => {
           this.items = response.data;
           this.filteredItems = this.items; // Initialize filteredItems with all items
           this.isLoading = false;
         })
-        .catch(error => {
-          console.error('Error fetching products:', error);
+        .catch((error) => {
+          console.error("Error fetching products:", error);
           this.isLoading = false;
         });
-    },
-    filterByAll() {
-      this.filteredItems = this.items; // Reset to all items
-    },
-    filterByRecentlyViewed() {
-      // Example: Filter by items with rating greater than 4.0
-      this.filteredItems = this.items.filter(item => item.rating > 4.0);
-    },
-    filterByFavorites() {
-      // Example: Filter by items with 'favorite' flag set
-      this.filteredItems = this.items.filter(item => item.favorite);
-    },
-    searchProducts() {
-      if (this.searchQuery.trim() === '') {
-        this.filteredItems = this.items; // Reset to all items if search query is empty
-      } else {
-        // Filter items based on searchQuery
-        this.filteredItems = this.items.filter(item =>
-          item.title.toLowerCase().includes(this.searchQuery.toLowerCase())
-        );
-      }
     },
     navigateBack() {
       this.$router.go(-1); // Navigate back to previous page
